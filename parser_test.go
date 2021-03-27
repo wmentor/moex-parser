c7e7341f0b8fa5b29978185a2fd180920ce36dea
Mikhail Kirillov <wmentor@mail.ru>
2021-03-27 21:32:00 +0300

    issue #1 optimize test

--- parser_test.go
+++ parser_test.go
@@ -1,23 +1,13 @@
 package parser
 
 import (
-	"encoding/json"
-	"fmt"
 	"testing"
 )
 
 func TestParser(t *testing.T) {
 
-	shares, trades, err := Get()
+	_, _, err := Get()
 	if err != nil {
 		t.Fatal(err)
-	}
-
-	fmt.Println(shares)
-	fmt.Println(trades)
-
-	for _, v := range trades {
-		data, _ := json.Marshal(v)
-		fmt.Println(string(data))
 	}
 }
